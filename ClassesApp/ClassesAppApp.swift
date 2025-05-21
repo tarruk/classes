dd9fa75841f5f2db9095787bb11d924a270976f1
Tarek Radovan <tarekradovan@Tareks-MacBook-Pro.local>
2025-05-21 11:42:41 -0400

    prepare project

---
+++ ClassesApp/ClassesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassesAppApp.swift
+//  ClassesApp
+//
+//  Created by Tarek Radovan on 21/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ClassesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
